cd0f9ec88361e779bf5a817933731381f6f4035e
zhaoti <1275737009@qq.com>
2025-12-15 03:42:50 +0800

    1

---
+++ C++/study/3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    bool flag = true;
+    bool flag2 = -1;
+    bool flag3 = 0;
+
+    cout << flag << " " << flag2 << " " << flag3 << endl;
+    
+    return 0;
+}
