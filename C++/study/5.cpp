c52c09c69e159675db817250f0afa0d6e0ac94b5
zhaoti <1275737009@qq.com>
2025-12-15 03:59:57 +0800

    1

--- C++/study/5.cpp
+++ C++/study/5.cpp
@@ -5,10 +5,17 @@ using namespace std;
 int main()
 {
     string s = "hello";
-    string s2 = "world";
+    string s2 = "world!";
     string s3 = s + " " + s2;
 
+    getline(cin, s);
+
+    cout << s << endl;
     cout << s3 << endl;
+    cout << s3.length() << endl;
+
+    string s4 = s3.substr(0, 7);
+    cout << s4 << endl;
 
     return 0;
 }
