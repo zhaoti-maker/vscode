ddb2e3bafd05e829dff7e745450fcd7158a3f6ce
zhaoti <1275737009@qq.com>
2025-12-15 03:48:55 +0800

    1

---
+++ C++/study/5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string s = "hello";
+    string s2 = "world";
+    string s3 = s + " " + s2;
+
+    cout << s3 << endl;
+
+    return 0;
+}
