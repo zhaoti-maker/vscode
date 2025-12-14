3c464031a0c40cd963a908de2068efb2be106295
zhaoti <1275737009@qq.com>
2025-12-15 03:32:28 +0800

    1

---
+++ C++/study/1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    printf("Input n:");
+    cin >> n;
+    cout << "hello world!" << " " << n++ << endl;
+
+    return 0;
+}
